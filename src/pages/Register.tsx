6a7244ea54952c5aaedada7b7a0ae8e16fa04b1e
Fady Malek <fadymalk1234@gmail.com>
2026-05-26 10:00:51 +0300

    Handle User Register With Feedback & Disable Button While Submitting

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,6 +5,9 @@ import { useForm, type SubmitHandler } from "react-hook-form";
 import { REGISTER_FORM } from "../data";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { registerSchema } from "../validation";
+import api from "../config/axios.config";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 interface IFormInput {
   username: string;
@@ -13,6 +16,7 @@ interface IFormInput {
 }
 
 const RegisterPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const {
     register,
     handleSubmit,
@@ -22,8 +26,31 @@ const RegisterPage = () => {
   });
 
   // ** Handlers
-  const onSubmit: SubmitHandler<IFormInput> = (data) =>
-    console.log("DATA", data);
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    setIsLoading(true);
+
+    try {
+      const { status } = await api.post("/auth/local/register", data);
+      if (status === 200) {
+        toast.success(
+          "You will be redirected to the login page in 4 seconds to log in.",
+          {
+            position: "bottom-center",
+            duration: 4000,
+            style: {
+              backgroundColor: "black",
+              color: "white",
+              width: "fitcontet",
+            },
+          },
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   // ** Renders
   const renderRegisterForm = REGISTER_FORM.map(
@@ -51,7 +78,9 @@ const RegisterPage = () => {
       <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
         {renderRegisterForm}
 
-        <Button fullWidth>Register</Button>
+        <Button fullWidth isLoading={isLoading}>
+          Register
+        </Button>
       </form>
     </div>
   );
